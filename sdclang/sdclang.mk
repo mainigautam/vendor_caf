PixelExperience/vendor_aosp

vendor: Import sdclang configuration from PA
Change-Id: If1b72992019dfd76d1f59e7ca06f6b10c195c958

sdclang/sdclang.mk
@@ -1,0 +1,24 @@
+# Copyright (C) 2016-2017 ParanoidAndroid Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ifneq ($(HOST_OS),linux)
+ifneq ($(sdclang_already_warned),true)
+$(warning **********************************************)
+$(warning * SDCLANG is not supported on non-linux hosts.)
+$(warning **********************************************)
+sdclang_already_warned := true
+endif
+else
+SDCLANG_LTO_DEFS := vendor/pa/sdclang/sdllvm-lto-defs.mk
+endif

mainigautam/vendor_caf

Signed-off-by: Mohd Faraz <mohd.faraz.abc@gmail.com>

sdclang/sdclang.mk
@@ -1,0 +1,24 @@
+# Copyright (C) 2016-2017 ParanoidAndroid Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+ifneq ($(HOST_OS),linux)
+ifneq ($(sdclang_already_warned),true)
+$(warning **********************************************)
+$(warning * SDCLANG is not supported on non-linux hosts.)
+$(warning **********************************************)
+sdclang_already_warned := true
+endif
+else
+SDCLANG_LTO_DEFS := vendor/aosp/sdclang/sdllvm-lto-defs.mk
+endif