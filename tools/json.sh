a08455ecaa129fcb967c95c60ceef0c23aecd205
inderpreetsinghb <inderpreetbhangu2@gmail.com>
2019-07-27 11:39:48 +0000

    Add CAF TYPE

--- tools/json.sh
+++ tools/json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "{ \"response\": [ { \"datetime\": $(grep ro\.build\.date\.utc $OUT/system/build.prop | cut -d= -f2), \"filename\": \"$(basename $(ls $OUT/CAF*.zip))\", \"id\": \"$((sha256sum $OUT/CAF*.zip) | cut -d ' ' -f1)\", \"romtype\": \"ALPHA\", \"size\": $(stat -c%s $OUT/CAF*.zip), \"version\": \"9.0-ALPHA\"  }]}" | tee $OUT/update
+echo "{ \"response\": [ { \"datetime\": $(grep ro\.build\.date\.utc $OUT/system/build.prop | cut -d= -f2), \"filename\": \"$(basename $(ls $OUT/CAF*.zip))\", \"id\": \"$((sha256sum $OUT/CAF*.zip) | cut -d ' ' -f1)\", \"version\": \"9.0-ALPHA\"  }]}" | tee $OUT/update
 
