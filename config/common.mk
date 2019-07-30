ReloadedOS/vendor_reloaded

vendor: prebuilt: Import backuptool from Lineage
Change-Id: I03d7bf70e41cd00e3abf83f0d3729f72aac6927e

configs/common.mk
@@ -12,6 +12,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Include support for GApps backup
+PRODUCT_COPY_FILES += \
+    vendor/wave/prebuilt/bin/backuptool.sh:install/bin/backuptool.sh \
+    vendor/wave/prebuilt/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/wave/prebuilt/bin/50-backuptool.sh:system/addon.d/50-backuptool.sh
+
+ifeq ($(AB_OTA_UPDATER),true)
+PRODUCT_COPY_FILES += \
+    vendor/wave/prebuilt/bin/backuptool_ab.sh:system/bin/backuptool_ab.sh \
+    vendor/wave/prebuilt/bin/backuptool_ab.functions:system/bin/backuptool_ab.functions \
+    vendor/wave/prebuilt/bin/backuptool_postinstall.sh:system/bin/backuptool_postinstall.sh
+endif
+
 # Include hostapd configuration
 PRODUCT_COPY_FILES += \
     vendor/wave/prebuilt/etc/hostapd/hostapd_default.conf:system/etc/hostapd/hostapd_default.conf \

mainigautam/vendor_caf

Signed-off-by: Mohd Faraz <mohd.faraz.abc@gmail.com>

config/common.mk
@@ -6,6 +6,19 @@ else
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.adb.secure=0
 endif
 
+# Include support for GApps backup
+PRODUCT_COPY_FILES += \
+    vendor/caf/prebuilt/bin/backuptool.sh:install/bin/backuptool.sh \
+    vendor/caf/prebuilt/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/caf/prebuilt/bin/50-backuptool.sh:system/addon.d/50-backuptool.sh
+
+ifeq ($(AB_OTA_UPDATER),true)
+PRODUCT_COPY_FILES += \
+    vendor/caf/prebuilt/bin/backuptool_ab.sh:system/bin/backuptool_ab.sh \
+    vendor/caf/prebuilt/bin/backuptool_ab.functions:system/bin/backuptool_ab.functions \
+    vendor/caf/prebuilt/bin/backuptool_postinstall.sh:system/bin/backuptool_postinstall.sh
+endif
+
 # Build Snapdragon apps
 PRODUCT_PACKAGES += \
     SnapdragonGallery \